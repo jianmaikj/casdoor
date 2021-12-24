5ee94310c4413cec8352fa5efc349e3472bf43ce
jianmaikj <75167751+jianmaikj@users.noreply.github.com>
2021-12-24 17:12:21 +0800

    feat: add api verification method:apiSign
    feat: add check-account-exist API
    
    Revert "feat: add api verification method:apiSign"
    
    This reverts commit 094eaf29f652307f5be59e9e183dd348738de28b.
    
    fix: CheckAccountExist

--- routers/router.go
+++ routers/router.go
@@ -50,6 +50,7 @@ func initAPI() {
 
 	beego.Router("/api/signup", &controllers.ApiController{}, "POST:Signup")
 	beego.Router("/api/login", &controllers.ApiController{}, "POST:Login")
+	beego.Router("/api/check-account", &controllers.ApiController{}, "POST:CheckAccountExist")
 	beego.Router("/api/get-app-login", &controllers.ApiController{}, "GET:GetApplicationLogin")
 	beego.Router("/api/get-dashboard", &controllers.ApiController{}, "GET:GetDashboard")
 	beego.Router("/api/logout", &controllers.ApiController{}, "GET,POST:Logout")
